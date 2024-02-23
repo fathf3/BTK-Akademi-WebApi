87559e9145b03db0e9f4939e03ca02ffffeeb2d6
fathf3 <fatih-348@hotmail.com>
2024-02-23 14:18:05 +0300

    Put, Delete, Patch

--- BookDemo/Controllers/BooksController.cs
+++ BookDemo/Controllers/BooksController.cs
@@ -1,6 +1,7 @@
 ﻿using BookDemo.Data;
 using BookDemo.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using AppContext = BookDemo.Data.AppContext;
 
@@ -46,13 +47,53 @@ namespace BookDemo.Controllers
 			}
 			
 		}
+		[HttpPut("{id:int}")]
+		public IActionResult UpdateOneBook([FromRoute(Name ="id")]int id,[FromBody] Book book)
+		{
+			var entity = AppContext
+				.Books
+				.Find(b => b.Id == id);	
 
+			if(entity is null)
+				return NotFound();
+			AppContext.Books.Remove(entity);
+			
+			book.Id = entity.Id;
+			AppContext.Books.Add(book);
+			return Ok(book);
+		}
 
+		[HttpDelete]
+		public IActionResult DeleteAllBooks()
+		{
+			
+			AppContext.Books.Clear();
+			return NoContent(); // 204
+		}
 
+		[HttpDelete("{id:int}")]
+		public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
+		{
+			var book = AppContext.Books.Find(b => b.Id == id);
+			if(book is null)
+				return NotFound(new
+				{
+					statusCode = 404,
+					message = $"Book with id : {id} could not found"
+				}); // 404
+			AppContext.Books.Remove(book);
+			return Ok($"Book id : {id} deleted");
+		}
 
+        [HttpPatch("{id:int}")]
+        public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] JsonPatchDocument<Book> bookPatch)
+		{
+			var entity = AppContext.Books.Find(b => b.Id == id);
+			if(entity is null)
+				return NotFound(); //404
+			bookPatch.ApplyTo(entity);
+			return NoContent(); // 204
+		} 
 
-
-
-
-	}
+    }
 }
