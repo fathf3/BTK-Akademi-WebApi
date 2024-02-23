34752ad6a2cd73e13ea068cf1d99f6eab518b0c0
fathf3 <fatih-348@hotmail.com>
2024-02-23 13:43:10 +0300

    Add project files.

---
+++ BookDemo/Controllers/BooksController.cs
@@ -0,0 +1,58 @@
+﻿using BookDemo.Data;
+using BookDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AppContext = BookDemo.Data.AppContext;
+
+namespace BookDemo.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BooksController : ControllerBase
+	{
+
+		[HttpGet]
+		public IActionResult GetAllBooks()
+		{
+			var books = AppContext.Books;
+			return Ok(books);
+		}
+		[HttpGet("{id:int}")]
+		public IActionResult GetOneBook([FromRoute(Name = "id")]int id)
+		{ 
+			var book = AppContext
+				.Books
+				.Where(b => b.Id.Equals(id))
+				.SingleOrDefault();
+			if (book == null)
+				return NotFound($"Bu id : {id} numralı kitap bulunamadı"); 
+			return Ok(book);
+		}
+		[HttpPost]
+		public IActionResult CreateOneBook([FromBody]Book book)
+		{
+			try
+			{
+				if (book is null)
+					return BadRequest(); //400
+
+				AppContext.Books.Add(book);
+				return StatusCode(201, book);
+
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+			
+		}
+
+
+
+
+
+
+
+
+	}
+}
---
+++ BookDemo/Data/AppContext.cs
@@ -0,0 +1,34 @@
+﻿using BookDemo.Models;
+
+namespace BookDemo.Data
+{
+	public static class AppContext
+	{
+        public static List<Book> Books {  get; set; } 
+        static AppContext()
+        {
+            Books = new List<Book>()
+            {
+                new Book
+                {
+                    Id = 1,
+                    Title = "Kitap 1",
+                    Price = 10,
+                },
+				new Book
+				{
+					Id = 2,
+					Title = "Kitap 2",
+					Price = 20,
+				},
+				new Book
+				{
+					Id = 3,
+					Title = "Kitap 3",
+					Price = 30,
+				},
+			};
+        }
+
+    }
+}
