018fabd721259e328ae973a67a004ebab38cf128
fathf3 <fatih-348@hotmail.com>
2024-02-25 02:56:34 +0300

    Global exception handler servisi ve middleware hazırlandı

---
+++ Entities/Exceptions/BookNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities.Exceptions
+{
+	public sealed class BookNotFoundException : NotFoundException
+    {
+		// Bu class dan kalıtım yapılamaz
+        public BookNotFoundException(int id) : base($"The book with id {id} could not found.")
+        {
+
+        }
+	}
+
+
+}
