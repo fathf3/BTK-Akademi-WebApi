84e346bc24c66412dcc790c1df59406c8b4c80f3
fathf3 <fatih-348@hotmail.com>
2024-02-25 23:23:00 +0300

    Accept Header, Serializable, Custom Formatters, IMvcBuilder

---
+++ BookDemo/Extensions/IMvcBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using BookDemo.Utilities.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Entities.Exceptions
+{
+	public static class IMvcBuilderExtensions
+	{
+		public static IMvcBuilder AddCustomCvsFormatter(this IMvcBuilder builder)
+		=>	builder.AddMvcOptions(config => 
+			config.OutputFormatters
+			.Add(new CsvOutputFormatter()));
+		
+	}
+}
---
+++ Entities/DataTransferObjects/Book/BookDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities.DataTransferObjects.Book
+{
+
+	
+	public record BookDto
+	{
+		// bu kullanımda [Serializable] kullanmaya gerek kalmaz
+		public int Id { get; set; }
+        public String Title { get; set; }
+        public decimal Price { get; set; }
+    }
+	
+}
