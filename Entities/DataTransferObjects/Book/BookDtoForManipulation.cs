cc118c06912f37700b5f01103df524f94b80dd94
fathf3 <fatih-348@hotmail.com>
2024-02-26 19:02:59 +0300

    Validation - ModelState- Custom Attributes- IValidationObject

---
+++ Entities/DataTransferObjects/Book/BookDtoForManipulation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects.Book
+{
+	public abstract record BookDtoForManipulation
+	{
+        [Required(ErrorMessage ="Title is a required field.")]
+        [MinLength(2, ErrorMessage ="Title must consist of least 2 characters")]
+        [MaxLength(100, ErrorMessage ="Title must consist of maximum 2 characters")]
+        public String Title { get; set; }
+
+		[Required(ErrorMessage = "Price is a required field.")]
+		[Range(10,10000)]
+        public decimal Price { get; set; }
+    }
+}
--- Presentation/Controllers/BooksController.cs
+++ Presentation/Controllers/BooksController.cs
@@ -48,29 +48,28 @@ namespace Presentation.Controllers
 		}
 
 		[HttpPost]
-		public IActionResult CreateOneBook([FromBody] Book book)
+		public IActionResult CreateOneBook([FromBody] BookDtoForInsertion bookDto)
 		{
-			try
-			{
-				if (book is null)
-					return BadRequest(); //400
+			if (bookDto is null)
+				return BadRequest(); //400
 
-				_manager.BookService.CreateOneBook(book);
+			// Valid degilse islemi birak
+			if(!ModelState.IsValid)
+				return UnprocessableEntity(ModelState);
 
-				return StatusCode(201, book);
-
-			}
-			catch (Exception ex)
-			{
-				return BadRequest(ex.Message);
-			}
+			var book =_manager.BookService.CreateOneBook(bookDto);
+			return StatusCode(201, book);
 
 		}
 
 		[HttpPut("{id:int}")]
 		public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] BookDtoForUpdate bookDto)
 		{
-			_manager.BookService.UpdateOneBook(id, bookDto, true);
+			if(bookDto is null)
+				return BadRequest();
+			if(!ModelState.IsValid)
+				return UnprocessableEntity(ModelState);
+			_manager.BookService.UpdateOneBook(id, bookDto, false);
 
 			return Ok(bookDto);
 		}
@@ -86,12 +85,17 @@ namespace Presentation.Controllers
 		}
 		
 		[HttpPatch("{id:int}")]
-		public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] JsonPatchDocument<Book> bookPatch)
+		public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] JsonPatchDocument<BookDto> bookPatch)
 		{
-			var entity = _manager.BookService.GetOneBookById(id, true);
+			var bookDto = _manager.BookService.GetOneBookById(id, true);
 			
-			bookPatch.ApplyTo(entity);
-			_manager.BookService.UpdateOneBook(id, new BookDtoForUpdate(entity.Id, entity.Title, entity.Price), true);
+			bookPatch.ApplyTo(bookDto);
+			_manager.BookService.UpdateOneBook(id,
+				new BookDtoForUpdate(){
+					Id = bookDto.Id,
+					Title = bookDto.Title,
+					Price = bookDto.Price,
+				},true);
 			return NoContent();
 		}
 	}
